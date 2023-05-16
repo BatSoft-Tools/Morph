97593e18827feaae62d167a193e16515364f4634
Peter Thönell <peter@batsoft.com.au>
2023-05-16 23:52:44 +0800

    Morph Daemon is installable/uninstallable.

---
+++ Implementation/CSharp/Install Morph Daemon in Debug directory.bat
@@ -0,0 +1,5 @@
+cd C:\Windows\Microsoft.NET\Framework64\v4.0.30319
+
+InstallUtil.exe C:\Dev\BatSoft\Morph\Implementation\CSharp\bin\Debug\Morph.Daemon.exe
+
+pause
---
+++ Implementation/CSharp/Uninstall Morph Daemon in Debug directory.bat
@@ -0,0 +1,5 @@
+cd C:\Windows\Microsoft.NET\Framework64\v4.0.30319
+
+InstallUtil.exe /u C:\Dev\BatSoft\Morph\Implementation\CSharp\bin\Debug\Morph.Daemon.exe
+
+pause
