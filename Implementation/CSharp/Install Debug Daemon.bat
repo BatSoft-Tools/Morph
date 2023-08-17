4f5d8da30cf2b01f483c5648154309cc0e9981a6
Peter Thönell <peter@batsoft.com.au>
2023-08-18 00:19:34 +0800

    Morph works in VS 2017

---
+++ Implementation/CSharp/Install Debug Daemon.bat
@@ -0,0 +1,2 @@
+"C:\Windows\Microsoft.NET\Framework\v4.0.30319\InstallUtil.exe" /install %~dp0\bin\Debug\Morph.Daemon.exe
+pause
---
+++ Implementation/CSharp/Uninstall Debug Daemon.bat
@@ -0,0 +1,2 @@
+"C:\Windows\Microsoft.NET\Framework\v4.0.30319\InstallUtil.exe" /uninstall %~dp0\bin\Debug\Morph.Daemon.exe
+pause
