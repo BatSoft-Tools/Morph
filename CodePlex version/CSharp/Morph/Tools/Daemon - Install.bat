ba384a18e04c906a48f25bbe016bcf14b61481a5
Peter Thönell <peter@batsoft.com.au>
2024-01-07 18:45:18 +0800

    Tidy up

--- CodePlex version/CSharp/Morph/Tools/Daemon - Install.bat
+++ CodePlex version/CSharp/Morph/Tools/Daemon - Install.bat
@@ -1,2 +1,3 @@
-"C:\Windows\Microsoft.NET\Framework\v4.0.30319\InstallUtil.exe" /install "..\Morph.Daemon\bin\Debug\Morph.Daemon.exe"
+"C:\Windows\Microsoft.NET\Framework\v4.0.30319\InstallUtil.exe" /install "%~dp0\..\Morph.Daemon\bin\Debug\Morph.Daemon.exe"
+pause
 del .\InstallUtil.InstallLog
--- CodePlex version/CSharp/Morph/Tools/Daemon - Uninstall.bat
+++ CodePlex version/CSharp/Morph/Tools/Daemon - Uninstall.bat
@@ -1,2 +1,3 @@
-"C:\Windows\Microsoft.NET\Framework\v4.0.30319\InstallUtil.exe" /uninstall "..\Morph.Daemon\bin\Debug\Morph.Daemon.exe"
+"C:\Windows\Microsoft.NET\Framework\v4.0.30319\InstallUtil.exe" /uninstall "%~dp0\..\Morph.Daemon\bin\Debug\Morph.Daemon.exe"
+pause
 del .\InstallUtil.InstallLog
